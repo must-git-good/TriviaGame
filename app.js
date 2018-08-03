48cc95ec1a932344a0878063fe9ff221714be81a
Kenny <kenny.fickling@gmail.com>
2018-08-03 00:09:13 -0500

    Buttons made and linked to right/wrong answer behavior.

--- app.js
+++ app.js
@@ -8,7 +8,10 @@
 var question = [
     {
         q: "A question string?",
-        a: [null, "answer", "b", "This is not right", "Last choice"],
+        a:  {
+            Button: [null, "A.", "B.", "C.", "D."],
+            Choices: [null, "answer", "b", "This is not right", "Last choice"]
+            },
         correctAnswer: 3,
         userCorrect: false,
         diff: 10,
@@ -16,18 +19,34 @@ var question = [
 
     }, {
         q: "Does this work?",
-        a: [null, "Yes", "No", "Maybe", "Here's hoping..."],
+        a:  {
+            Button: [null, "A.", "B.", "C.", "D."],
+            Choices: [null, "Yes", "No", "Maybe", "Here's hoping..."]
+            },
         correctAnswer: 4,
         userCorrect: false,
         diff: 7,
-        h: "We're in development",
+        hint: "We're in development",
     }, {
         q: "True, False, Frue or Talse?",
-        a: [null, "Yes", "What?!", "True", "False"],
+        a:  {
+            Button: [null, "A.", "B.", "C.", "D."],
+            Choices: [null, "Yes", "What?!", "True", "False"]
+            },
         userCorrect: false,
         correctAnswer: 2,
         diff: 1,
-        h: "A genuine reaction.",
+        hint: "A genuine reaction.",
+    }, {
+        q: "True, False, Frue or Talse?",
+        a:  {
+            Button: [null, "A.", "B.", "C.", "D."],
+            Choices: [null, "Yes", "What?!", "True", "False"]
+            },
+        userCorrect: false,
+        correctAnswer: 2,
+        diff: 1,
+        hint: "A genuine reaction.",
     }
     //make a bunch more of these. find a 3rd party so I don't have to think of tons of questions.
 ];
@@ -36,8 +55,8 @@ var theQuestion = "";
 var theAnswer = -1;
 var answerChoices = [];
 //
-
-
+var correctAnswers = 0;
+var wrongGuesses = 0;
 
 
 
@@ -56,7 +75,7 @@ function assignAnswer(questionNumber) {
 }
 
 function listAnswers(questionNumber) {
-    answerChoices = question[questionNumber].a;
+    answerChoices = question[questionNumber].a.Choices;
 }
 
 function getCurrent(questionNumber) {
@@ -68,8 +87,13 @@ function getCurrent(questionNumber) {
 //Let's create the html to fill, dynamically:
 
 
+
+
 function populateQuestions(question){
     numOfQuestionsDesired = question.length    //Setting this variable to "all questions" for now, but can be dynamic.
+
+    
+
 
 for (var i = 0; i<numOfQuestionsDesired; i++){
 
@@ -78,7 +102,7 @@ for (var i = 0; i<numOfQuestionsDesired; i++){
     var contentArea = $("<div>");
     var showQuestion = $("<span>");
     var answerArea = $("<div>");
-    var makeButton = $("<button>")
+    var makeButton = $("<button"+question[i].a.Choices.length+">")
     
     getCurrent(i);
     contentArea.addClass("content-hold");
@@ -87,19 +111,38 @@ for (var i = 0; i<numOfQuestionsDesired; i++){
 
         showQuestion.addClass("question");
         showQuestion.attr(("id", "question-number-"+[i]));
+        
         showQuestion.html(theQuestion);
 
     contentArea.append(answerArea);
         
         answerArea.addClass("answers");
         answerArea.attr("id", "answers-to-"+[i]);
-            for(var j= 1; j<(question.a.length-1); j++){
-                makeButton.attr("id", "answer-"+[j]);
-                makeButton.attr("value", [j]);
-                answerArea.append(makeButton);   /// HERE WE ARE WORKING
-        }
+            for (var j = 1; j<question[i].a.Choices.length; j++){
+        answerArea.append("<button class='btn' value="+[i]+[j]+" id=" + [question[i].a.Button[j]] + ">" + " " + question[i].a.Button[j]);
+        answerArea.append("  "+question[i].a.Choices[j]+"<br><br>");
+        }           
+
+        console.log(question[i]);
+        console.log(question[i].a);
+        console.log(question[i].a.Choices);
+        console.log(question[i].a.Choices.length);
+         
+        
+        
        
-    
+        
+
+
+
+
+
+        // for(var j= 1; j<(question[0].a.length); j++){
+        //     makeButton.attr("id", "answer-"+[j]);
+        //     makeButton.attr("value", [j]);
+        //     makeButton.text([j]+".")
+        //     answerArea.append(makeButton);   
+        // }
 
    
 //
@@ -119,6 +162,15 @@ for (var i = 0; i<numOfQuestionsDesired; i++){
 } //end of for loop
 }; //end of populate function
 
+
+$(document).ready(function(){
+
+
+populateQuestions(question);
+
+$(".content-hold").hide();
+
+});//end of 'ready'
 
 
 
@@ -156,9 +208,59 @@ for (var i = 0; i<numOfQuestionsDesired; i++){
 
 
 document.onkeyup = function () {
-    populateQuestions(question);
+    var counter = -1;
+    window.setInterval(function(){
+    $("#section-number-"+(counter-1)).hide();
+    var revealNextQuestion = $("#section-number-"+counter).show();
+    console.log(counter);
+    counter++;
+    console.log(counter);
+}, 5000);
 
-    console.log("Info is current: "+ theQuestion+theAnswer+answerChoices);
+
+
+$(".btn").on("click", function(){
+    console.log(this);
+    console.log("This value:  " +$(this).val());
+
+                
+
+    var coordinates = $(this).val();
+    var code =   coordinates.split("");
+    var currentQuestion = code[0];
+    var clickedAnswer = code[1];
+
+        console.log(code);
+        console.log("Question? " + currentQuestion);
+        console.log("My guess:  "+ clickedAnswer);
+
+
+        console.log("True1 " +clickedAnswer);
+        console.log("True2 " +question[currentQuestion].correctAnswer);
+
+    if (clickedAnswer == question[currentQuestion].correctAnswer){
+        alert("You earned a point.");
+        correctAnswer++;
+            //RIGHT ANSWER EVENTS HERE.
+    } else if (clickedAnswer != question[currentQuestion].correctAnswer){
+        alert("You got it WRONG!");
+        wrongGuesses++;
+            //WRONG GUESS EVENTS HERE.
+    }       
+
+
+
+})
+
+// $(".btn").on("click", function(){
+//     alert("The click works!");
+// })
+
+
+// $("#section-number-1").show();
+
+
+    console.log("OnKey has activated");
 
 
 }
